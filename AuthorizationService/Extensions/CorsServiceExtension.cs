9454d52a125740e460886c75b18fdfb6b7b69ce4
mattix10 <mateusz.salamon10@gmail.com>
2024-07-27 21:27:24 +0200

    update authorization service solution

---
+++ AuthorizationService/Extensions/CorsServiceExtension.cs
@@ -0,0 +1,19 @@
+namespace AuthorizationService.Extensions;
+public static class CorsServiceExtension
+{
+    public static IServiceCollection AddCorsServices(this IServiceCollection services)
+    {
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll",
+                policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyHeader()
+                          .AllowAnyMethod();
+                });
+        });
+        return services;
+    }
+}
